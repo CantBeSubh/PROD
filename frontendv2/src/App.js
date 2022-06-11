e5a35534194c34e051eb79443f6b717902c974d8
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-06-11 07:50:29 +0530

    clean up.CRUD habits

--- frontendv2/src/App.js
+++ frontendv2/src/App.js
@@ -13,38 +13,6 @@ import {getHabitsQ} from './queries'
 function App() {
   const [habits, setHabits] = useState([]);
   const { data } = useQuery(getHabitsQ);
-
-  const upHabit=(id)=>{
-    setHabits(
-      habits.map(habit => {
-        if (habit.id === id) {
-          return {
-            ...habit,
-            up: habit.up+1
-          };
-        }
-        return habit;
-      })
-    );
-  }
-
-  const downHabit=(id)=>{
-    setHabits(
-      habits.map(habit => {
-        if (habit.id === id) {
-          return {
-            ...habit,
-            down: habit.down+1
-          };
-        }
-        return habit;
-      })
-    );
-  }
-
-  const removeHabit=(id)=> {
-    setHabits(habits.filter(habit => habit.id !== id));
-  }
   
   useEffect(()=>{
     if(data){
@@ -52,16 +20,11 @@ function App() {
     }
   },[data])
 
-
-
   return (
     <div className="App">
       <HabitForm />
       <HabitList 
       habits={habits} 
-      upHabit={upHabit}
-      downHabit={downHabit}
-      removeHabit={removeHabit}
       />
     </div>
   );
--- frontendv2/src/comps/HabitList.jsx
+++ frontendv2/src/comps/HabitList.jsx
@@ -1,15 +1,12 @@
 import Habit from "./Habit"
 
-const HabitList = ({habits,upHabit,downHabit,removeHabit}) => {
+const HabitList = ({habits}) => {
   return (
     <ul>
       {habits.map(habit=>
         <Habit
         key={habit.id}
         habit={habit}
-        upHabit={upHabit}
-        downHabit={downHabit}
-        removeHabit={removeHabit}
         />      
         )}
     </ul>

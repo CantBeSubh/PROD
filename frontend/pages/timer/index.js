e97c2c33956e51b5049306c18d780f644560e359
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-06-16 17:20:12 +0530

    idk

--- frontend/pages/timer/index.js
+++ frontend/pages/timer/index.js
@@ -5,51 +5,56 @@ const MS_TO_HR= 3600000
 const MS_TO_MIN=60000
 const MS_TO_SEC=1000
 
+const spitTime=ms=>{
+    let hr=Math.floor(ms/MS_TO_HR)
+    let min=Math.floor(ms/MS_TO_MIN)
+    let sec=Math.floor(ms/MS_TO_SEC)
+    ms=Math.floor(((ms/1000)%1)*1000)
+    sec%=60
+    min%=60
+    const time=`${hr}hours:${min}min:${sec}seconds:${ms}ms`
+    return time
+}
+
+const time=t=>{
+    return `${t.getDate()}/${t.getMonth()+1}/${t.getFullYear()} | ${t.getHours()}:${t.getMinutes()}:${t.getSeconds()}`
+}
+
 const index = () => {
-    const [date,setDate]=useState()
-    const [start,setStart]=useState()
-    let current = new Date()
-    const [isPaused,setIsPaused]=useState(false)
+    const [date,setDate]=useState('')
+    const [start,setStart]=useState(new Date())
+    const [timeSpent,setTimeSpent]=useState('')
+    const [isPaused,setIsPaused]=useState(true)
     const [id,setId]=useState()
+    let current = new Date()
 
-    const spitTime=ms=>{
-        let hr=Math.floor(ms/MS_TO_HR)
-        let min=Math.floor(ms/MS_TO_MIN)
-        let sec=Math.floor(ms/MS_TO_SEC)
-        ms=Math.floor(((ms/1000)%1)*1000)
-        sec%=60
-        min%=60
-        const time=`${hr}hours:${min}min:${sec}seconds:${ms}ms`
-        return time
-    }
-
-    useEffect(()=>{
-        setDate(`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()} | ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`)
-    },[])
-
-    useEffect(()=>{
-        console.log(start)
-    },[start])
-    
     const handleStart=()=>{
         setStart(new Date())
+        setDate(time(new Date()))
+        setIsPaused(false)
+        setTimeSpent('')
         setId(setInterval(()=>{
-            current.setTime(Date.now());
-            setDate(`${current.getDate()}/${current.getMonth()+1}/${current.getFullYear()} | ${current.getHours()}:${current.getMinutes()}:${current.getSeconds()}`)
+            current = new Date();
+            console.log(start)
+            setDate(spitTime(current-start))
         },1000))
     }
 
     const handleStop=()=>{
-        current=Date.now()-start
         clearInterval(id)
-        console.log(spitTime(current))
+        setDate('')
+        setId('')
+        setIsPaused(true)
+        setTimeSpent(spitTime(Date.now()-start))
+        setStart()
     }
 
     return (
         <div>
-            Start timer = {date}
-            <button onClick={handleStart}>+</button>
-            <button onClick={handleStop}>X</button>
+            {!isPaused && `Start timer = ${date}`}
+            {isPaused && <button onClick={handleStart}>+</button>}
+            {!isPaused && <button onClick={handleStop}>X</button>}
+            {isPaused && `timeSpent:${timeSpent}`}
         </div>
     )
 }

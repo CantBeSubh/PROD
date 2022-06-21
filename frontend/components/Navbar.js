97d146c09acffbe7e7bd1ec934b1dd8729bb2fd3
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-06-22 00:23:38 +0530

    more css

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -20,14 +20,14 @@ const Navbar = () => {
             <div className="logoNav">
                 <Image
                     src='/favicon.ico'
-                    width={128 / 4}
-                    height={128 / 4}
+                    width={50}
+                    height={50}
                     alt='logo'
                 />
             </div>
-            <Link href='/'><a>Home</a></Link>
-            <Link href='/about'><a>About</a></Link>
-            <Link href='/timer'><a>Timer</a></Link>
+            <Link href='/' ><a className={styles.btn}>Home</a></Link>
+            <Link href='/about' ><a className={styles.btn}>About</a></Link>
+            <Link href='/timer' ><a className={styles.btn}>Timer</a></Link>
             {
                 auth ?
                     <div onClick={logout} className={styles.btn}>Logout</div>
--- frontend/pages/auth/index.js
+++ frontend/pages/auth/index.js
@@ -71,116 +71,118 @@ const index = () => {
     }
 
     return (
-        <div class="container">
+        <div className="section">
+            <div class="container">
 
-            <div>
-                <Head>
-                    <title>Auth</title>
-                </Head>
-            </div>
+                <div>
+                    <Head>
+                        <title>Auth</title>
+                    </Head>
+                </div>
 
-            <div class="section text-center">
+                <div class="section text-center">
 
-                <h6 class="mb-0 pb-3"><span>Log In</span><span>Sign Up</span></h6>
+                    <h6 class="mb-0 pb-3"><span>Log In</span><span>Sign Up</span></h6>
 
-                <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
-                <label for="reg-log"></label>
+                    <input class="checkbox" type="checkbox" id="reg-log" name="reg-log" />
+                    <label for="reg-log"></label>
 
-                <div class="card-3d-wrap mx-auto">
+                    <div class="card-3d-wrap mx-auto">
 
-                    <div class="card-3d-wrapper">
-                        <div class="card-front">
-                            <div class="center-wrap">
-                                <div class="section text-center">
-                                    <h4 class="mb-4 pb-3">Log In</h4>
+                        <div class="card-3d-wrapper">
+                            <div class="card-front">
+                                <div class="center-wrap">
+                                    <div class="section text-center">
+                                        <h4 class="mb-4 pb-3">Log In</h4>
 
-                                    <div class="form-group">
-                                        <Input
-                                            label='Your Mail'
-                                            type='email'
-                                            value={login.email}
-                                            onChange={e => setLogin({ ...login, email: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-at"></i>
+                                        <div class="form-group">
+                                            <Input
+                                                label='Your Mail'
+                                                type='email'
+                                                value={login.email}
+                                                onChange={e => setLogin({ ...login, email: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-at"></i>
+                                        </div>
+
+                                        <div class="form-group mt-2">
+                                            <Input
+                                                label='Password'
+                                                type={type}
+                                                value={login.password}
+                                                onChange={e => setLogin({ ...login, password: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-lock-alt"
+                                                onClick={() => setType(old => old == 'text' ? 'password' : 'text')}
+                                            ></i>
+                                        </div>
+
+                                        <Button onClick={handleLogin}>submit</Button>
+                                        <p class="mb-0 mt-4 text-center">
+                                            <a href="#0" class="link">
+                                                Forgot your password?
+                                            </a>
+                                        </p>
                                     </div>
-
-                                    <div class="form-group mt-2">
-                                        <Input
-                                            label='Password'
-                                            type={type}
-                                            value={login.password}
-                                            onChange={e => setLogin({ ...login, password: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-lock-alt"
-                                            onClick={() => setType(old => old == 'text' ? 'password' : 'text')}
-                                        ></i>
-                                    </div>
-
-                                    <Button onClick={handleLogin}>submit</Button>
-                                    <p class="mb-0 mt-4 text-center">
-                                        <a href="#0" class="link">
-                                            Forgot your password?
-                                        </a>
-                                    </p>
                                 </div>
                             </div>
-                        </div>
-                        <div class="card-back">
-                            <div class="center-wrap">
-                                <div class="section text-center">
-                                    <h4 class="mb-4 pb-3">Sign Up</h4>
+                            <div class="card-back">
+                                <div class="center-wrap">
+                                    <div class="section text-center">
+                                        <h4 class="mb-4 pb-3">Sign Up</h4>
 
-                                    <div class="form-group">
-                                        <Input
-                                            label='Name'
-                                            type='text'
-                                            value={signup.name}
-                                            onChange={e => setSignup({ ...signup, name: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-user"></i>
+                                        <div class="form-group">
+                                            <Input
+                                                label='Name'
+                                                type='text'
+                                                value={signup.name}
+                                                onChange={e => setSignup({ ...signup, name: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-user"></i>
+                                        </div>
+
+                                        <div class="form-group mt-2">
+                                            <Input
+                                                label='Username'
+                                                type='text'
+                                                value={signup.username}
+                                                onChange={e => setSignup({ ...signup, username: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-user"></i>
+                                        </div>
+
+                                        <div class="form-group mt-2">
+                                            <Input
+                                                label='Your Mail'
+                                                type='email'
+                                                value={signup.email}
+                                                onChange={e => setSignup({ ...signup, email: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-at"></i>
+                                        </div>
+
+                                        <div class="form-group mt-2">
+                                            <Input
+                                                label='Password'
+                                                type={type}
+                                                value={signup.password}
+                                                onChange={e => setSignup({ ...signup, password: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-lock-alt"></i>
+                                        </div>
+
+                                        <div class="form-group mt-2">
+                                            <Input
+                                                label='Confirm Password'
+                                                type={type}
+                                                value={signup.cPassword}
+                                                onChange={e => setSignup({ ...signup, cPassword: e.target.value })}
+                                            />
+                                            <i class="input-icon uil uil-lock-alt"></i>
+                                        </div>
+
+                                        <Button onClick={handleSignup}>submit</Button>
                                     </div>
-
-                                    <div class="form-group mt-2">
-                                        <Input
-                                            label='Username'
-                                            type='text'
-                                            value={signup.username}
-                                            onChange={e => setSignup({ ...signup, username: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-user"></i>
-                                    </div>
-
-                                    <div class="form-group mt-2">
-                                        <Input
-                                            label='Your Mail'
-                                            type='email'
-                                            value={signup.email}
-                                            onChange={e => setSignup({ ...signup, email: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-at"></i>
-                                    </div>
-
-                                    <div class="form-group mt-2">
-                                        <Input
-                                            label='Password'
-                                            type={type}
-                                            value={signup.password}
-                                            onChange={e => setSignup({ ...signup, password: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-lock-alt"></i>
-                                    </div>
-
-                                    <div class="form-group mt-2">
-                                        <Input
-                                            label='Confirm Password'
-                                            type={type}
-                                            value={signup.cPassword}
-                                            onChange={e => setSignup({ ...signup, cPassword: e.target.value })}
-                                        />
-                                        <i class="input-icon uil uil-lock-alt"></i>
-                                    </div>
-
-                                    <Button onClick={handleSignup}>submit</Button>
                                 </div>
                             </div>
                         </div>

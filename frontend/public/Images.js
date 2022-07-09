ffd2a78cd1a583a987e7677926e64d333e40a45b
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-07-09 14:11:27 +0530

    w

--- frontend/public/Images.js
+++ frontend/public/Images.js
@@ -2,8 +2,10 @@ const PixelCity = 'https://i.pinimg.com/originals/8d/b2/59/8db259720d98e98b667c3
 const PixelInterior1 = 'https://i.pinimg.com/originals/5e/17/e4/5e17e41a2778c39f9abee9fc19b67cc7.gif'
 const PixelInterior2 = 'https://i.pinimg.com/originals/22/be/2d/22be2dc87badd5fd1959b4e2b28bf2e5.gif'
 
+const PixelInterior3 = 'https://i.pinimg.com/originals/cb/71/a0/cb71a099b3ca06a3797f32f4af4c9011.gif'
 export {
     PixelCity,
     PixelInterior1,
-    PixelInterior2
+    PixelInterior2,
+    PixelInterior3
 }
